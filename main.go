fef9569f850bc02fffe3c39c4202d7608e4cf9f1
Ivan <aipyth.func@gmail.com>
2021-04-28 14:40:31 +0300

    PORT for heroku spec

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.html")
@@ -20,5 +26,5 @@ func main() {
 		c.HTML(http.StatusOK, "signin.html", nil)
 	})
 
-	router.Run(":8000")
+	router.Run(":" + port)
 }
